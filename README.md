d0f86e259e0bff9b29e9971c29324fcf5adf5249
Artem Oganesyan <artem.oganesyan@gmail.com>
2021-09-15 10:34:00 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# pcs-tech-s2-sample
+# Technology section - Sample application
+
+Requirements:
+1. Login area must be centered on screen and have width for about 15cm.
+2. Login and password fields must have labels.
+3. Page title must be named "Learning system".
+4. Button must be named "Login" and have grey backround color.
+5. After successfull login user should see welcome message.
+
+Test data:
+Login: test
+Password: test
---
+++ script.js
@@ -0,0 +1,19 @@
+function auth(login, password) {
+    var client = new HttpClient();
+    client.get("http://pcs-tech-sample-216626155.us-east-2.elb.amazonaws.com/auth?login="+login+"&password="+password+"", function(response) {
+        document.getElementById("result").innerHTML = response;
+    });
+}
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
